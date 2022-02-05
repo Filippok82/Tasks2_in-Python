790b7d55d2b9cbf3a48a1490b1cf404d3c97c98e
Irina Filippova <irina.filippova.mail@yandex.ru>
2022-02-05 21:37:25 +0300

    Upgate task29

--- semin03_04/Task_31.py
+++ semin03_04/Task_31.py
@@ -6,9 +6,9 @@ n = int(input('введите n:'))
 
 a=[]
 for i in range(2,n + 1):
-    if n % i == 0:
+    if n % i == 0:           
         for j in range(2,(i//2 + 1)):
-            if(i % j == 0):               
+            if(i % j == 0): 
                 break
         else:
             a.append(i)
