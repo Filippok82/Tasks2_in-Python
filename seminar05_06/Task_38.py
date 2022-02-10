15cbb63f76d0549ea1397a2da7ee303d6e35259b
Irina Filippova <irina.filippova.mail@yandex.ru>
2022-02-10 21:10:56 +0300

    lesson 04 task38

---
+++ seminar05_06/Task_38.py
@@ -0,0 +1,23 @@
+# 38.	Напишите программу, удаляющую из текста все слова содержащие "абв".
+
+# from functools import reduce
+# str='рапааисим фбварара абвгдх ваабвгр'
+# print(str)
+# str=str.split('абв')
+# str = reduce(lambda x,y: x+y ,[i for i in str])
+# print(str)
+
+
+#________________________________________________________________
+
+text='рапааисим фбварара абвгдх ваабвгр'
+print(text)
+tex=text.split()
+print(tex)
+for word in tex[:]:
+    if 'абв' in word:
+        tex.remove(word)
+print(tex)
+
+
+        
